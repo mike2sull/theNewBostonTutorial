a655239fb48d96b0bd8eda51ee0e1f435c46f8b9
mike2sull <mike2sull@gmail.com>
2016-06-29 22:43:31 -0400

    TheNewBoston Tutorial thru ep 23

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-buttcrack = 5
+fw = open('sample.txt', 'w')
+fw.write('Writing some stuff in my text file!\n')
+fw.write('And write something else on this line\n')
+fw.close()
 
-while buttcrack <= 10:
-    print(buttcrack)
-    buttcrack += 1
+fr = open('sample.txt', 'r')
+text = fr.read()
+fr.close()
+
+print(text)
