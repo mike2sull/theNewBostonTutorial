0486ac9e20f638f2d6ca53bd654811f9f8128c93
mike2sull <mike2sull@gmail.com>
2016-06-30 20:40:07 -0400

    TheNewBoston Tutorial thru ep 27

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
-fw = open('sample.txt', 'w')
-fw.write('Writing some stuff in my text file!\n')
-fw.write('And write something else on this line\n')
-fw.close()
+import requests
+from bs4 import BeautifulSoup
 
-fr = open('sample.txt', 'r')
-text = fr.read()
-fr.close()
+def forum_spider(max_pages):
+    page = 1
+    while page < max_pages:
+        url = 'https://thenewboston.com/forum/recent_activity.php?page=' + str(page)
+        source_code = requests.get(url).text
+        # plain_text = source_code.text
+        soup = BeautifulSoup(source_code, 'html.parser')
+        for link in soup.findAll('a', {'class': 'title text-semibold'}):
+            href = link.get('href')
+            title = link.string
+            formattedTitle = title.strip()
+            # print(href)
+            print('\nTitle: ', formattedTitle, '\n')
+            print('Usernames:\n'),
+            get_single_item_data(href)
+        page += 1
+        print("\npage:", page - 1)
 
-print(text)
+def get_single_item_data(item_url):
+    source_code = requests.get(item_url).text
+    soup = BeautifulSoup(source_code, 'html.parser')
+    # for posters in soup.findAll('a', {'class': 'user-name'}):
+        # username = posters.string
+        # formattedUsername = username.strip()
+        # print(formattedUsername)
+    for link in soup.findAll('a'):
+        href = link.get('href')
+        hrefSet = set()
+        hrefSet.add(href)
+        print(hrefSet)
+
+
+forum_spider(2)
